433af5f4ed656785fced4d12b64d31ca83b26fb7
Jewel Rana <tonujewel@gmail.com>
2020-10-27 19:12:25 +0600

    start working on search page

--- lib/screens/SearchScreen/search_screen.dart
+++ lib/screens/SearchScreen/search_screen.dart
@@ -10,31 +10,32 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            title: Center(child: Text("Search Screen")),
-          ),
-          body: Column(
-            children: [
-              SizedBox(
-                height: 30.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextField(
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.transparent),
-                            borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.transparent),
-                            borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Search",
-                        filled: true,
-                        fillColor: Colors.grey[200])),
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Center(child: Text("Search Screen")),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search",
+                      filled: true,
+                      fillColor: Colors.grey[200])),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
